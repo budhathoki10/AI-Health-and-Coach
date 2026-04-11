4c79f0175617ee8dfe99dea4fd9d6cfac6bd4781
budhathoki10 <budhathokikushal170@gmail.com>
2026-04-11 20:36:05 +0545

    .

--- Backend/app.js
+++ Backend/app.js
@@ -24,7 +24,10 @@ const TrackDashbaord= require("./Routes/TrackCalories.route")
 
 
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
+mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
     console.log("Connected to MongoDB")
 }
 ).catch((err) => {
